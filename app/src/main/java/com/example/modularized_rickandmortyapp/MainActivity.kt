6f5b7b300eaae88c6abcfc96824c5bb6c33a6bab
Shahzad Ansari <shahzadansari03@outlook.com>
2023-06-29 22:46:25 +0500

    Remove unused imports

--- app/src/main/java/com/example/modularized_rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/modularized_rickandmortyapp/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.modularized_rickandmortyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
--- character/ui-character-list/src/main/java/com/example/ui_character_list/ui/CharactersList.kt
+++ character/ui-character-list/src/main/java/com/example/ui_character_list/ui/CharactersList.kt
@@ -5,9 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
