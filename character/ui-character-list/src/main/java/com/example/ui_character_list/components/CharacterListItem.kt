f9d5ed9d0df729691e53c9ff0748350782f3f1fc
Shahzad Ansari <shahzadansari03@outlook.com>
2024-03-17 01:16:28 +0500

    Add tags in CharactersListItem()

--- character/ui-character-list/src/main/java/com/example/ui_character_list/components/CharacterListItem.kt
+++ character/ui-character-list/src/main/java/com/example/ui_character_list/components/CharacterListItem.kt
@@ -1,21 +1,28 @@
 package com.example.ui_character_list.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -62,16 +69,54 @@ fun CharacterListItem(
             }
             Spacer(modifier = Modifier.width(8.dp))
 
-            Text(text = character.name, style = MaterialTheme.typography.bodyLarge)
+            Text(
+                text = character.name,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.weight(1f),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
             Spacer(modifier = Modifier.width(8.dp))
+
+            Row(
+                modifier = Modifier.padding(end = 4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End)
+            ) {
+                Tag(label = character.status.name)
+                Tag(label = character.gender.name)
+            }
         }
+    }
+}
+
+@Composable
+private fun Tag(label: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.onPrimary)
+            .padding(vertical = 4.dp, horizontal = 8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = label, style = MaterialTheme.typography.labelSmall)
     }
 }
 
 @Previews
 @Composable
-fun PreviewCharacterListItem() {
+private fun CharacterListItemPreview() {
     ModularizedRickAndMortyAppTheme {
         CharacterListItem(character = Character.example, onCharacterSelected = {})
+    }
+}
+
+@Preview
+@Composable
+private fun TagPreview() {
+    ModularizedRickAndMortyAppTheme {
+        Box(modifier = Modifier.padding(8.dp)) {
+            Tag(label = "Alive", modifier = Modifier.align(Alignment.Center))
+        }
     }
 }
